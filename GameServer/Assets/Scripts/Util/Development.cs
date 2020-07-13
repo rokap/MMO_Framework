055c3ca9198cfd531bdf2200591cfe32bef2b6f0
David Kaplan <dave@rokap.com>
2020-07-13 02:26:10 -0400

    Created Helper Class

---
+++ GameServer/Assets/Scripts/Util/Development.cs
@@ -0,0 +1,15 @@
+﻿
+
+using UnityEngine;
+
+public class Development
+{
+    public static void Log(string message)
+    {
+        Debug.Log(message + "\n-------------------------------------------------");
+    }
+    public static void Divider()
+    {
+        Debug.Log("\n\n");
+    }
+}
