c79272fd1d712edb2da4fca8588f0467b89467e6
David Kaplan <dave@rokap.com>
2020-07-13 02:31:19 -0400

    Fixed Field Name

--- GameServer/Assets/Scripts/Server/Database/Active Records/NpcSpawn.cs
+++ GameServer/Assets/Scripts/Server/Database/Active Records/NpcSpawn.cs
@@ -13,7 +13,7 @@ public partial class Database
         public uint id;
         public uint npc_id;
         public uint npc_spawn_group_id;
-        public uint npc_spawn_point;
+        public uint npc_spawn_point_id;
         public float position_x;
         public float position_y;
         public float position_z;
@@ -21,11 +21,11 @@ public partial class Database
 
         public NpcSpawn() : base() { }
 
-        public NpcSpawn(uint npc_id, uint npc_spawn_group_id, uint npc_spawn_point, float position_x, float position_y, float position_z, float heading)
+        public NpcSpawn(uint npc_id, uint npc_spawn_group_id, uint npc_spawn_point_id, float position_x, float position_y, float position_z, float heading)
         {
             this.npc_id = npc_id;
             this.npc_spawn_group_id = npc_spawn_group_id;
-            this.npc_spawn_point = npc_spawn_point;
+            this.npc_spawn_point_id = npc_spawn_point_id;
             this.position_x = position_x;
             this.position_y = position_y;
             this.position_z = position_z;
